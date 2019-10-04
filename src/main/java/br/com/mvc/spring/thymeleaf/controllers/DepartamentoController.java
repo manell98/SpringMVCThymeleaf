b7e5ab5451dc1ad10aa0c1810dda329274e5e431
Emanuell Santos <dfmanu06@gmail.com>
2019-10-04 20:37:54 -0300

    Cadastro/listagem de Departamento

--- src/main/java/br/com/mvc/spring/thymeleaf/controllers/DepartamentoController.java
+++ src/main/java/br/com/mvc/spring/thymeleaf/controllers/DepartamentoController.java
@@ -1,22 +1,38 @@
 package br.com.mvc.spring.thymeleaf.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.mvc.spring.thymeleaf.domain.Departamento;
+import br.com.mvc.spring.thymeleaf.services.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService departamentoService;
 
 	@RequestMapping("cadastrar")
-	public String cadastrar() {
-		
+	public String cadastrar(Departamento departamento) {	
 		return "departamento/cadastro";
 	}
 	
 	@RequestMapping("listar")
-	public String listar() {
+	public String listar(ModelMap model) {	
+		model.addAttribute("departamentos", departamentoService.findAll());
 		
 		return "departamento/lista";
+	}
+	
+	@RequestMapping(value= "salvar", method=RequestMethod.POST)
+	public String salvar(Departamento departamento) {
+		departamentoService.insert(departamento);
+		
+		return "redirect:/departamentos/cadastrar";
 	}
 	
 }
