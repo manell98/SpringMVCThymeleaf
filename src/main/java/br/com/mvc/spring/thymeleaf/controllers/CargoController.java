db71c3eca23f56e28ddc53928defa5bbf9de1910
Emanuell Santos <dfmanu06@gmail.com>
2019-10-08 14:41:14 -0300

    Listagem de Cargos

--- src/main/java/br/com/mvc/spring/thymeleaf/controllers/CargoController.java
+++ src/main/java/br/com/mvc/spring/thymeleaf/controllers/CargoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,7 +32,8 @@ public class CargoController {
 	}
 	
 	@RequestMapping("listar")
-	public String listar() {
+	public String listar(ModelMap model) {	
+		model.addAttribute("cargos", service.findAll());
 		
 		return "cargo/lista";
 	}
