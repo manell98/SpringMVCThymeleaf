d4a06047559e4b25c3ab77e9faae068d0c64b926
Emanuell Santos <dfmanu06@gmail.com>
2019-10-08 16:52:05 -0300

    Editar Cargos

--- src/main/java/br/com/mvc/spring/thymeleaf/controllers/CargoController.java
+++ src/main/java/br/com/mvc/spring/thymeleaf/controllers/CargoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -50,6 +51,21 @@ public class CargoController {
 	public List<Departamento> listaDepartamentos() {
 		
 		return departamentoService.findAll();
+	}
+	
+	@RequestMapping("editar/{id}")
+	public String find(@PathVariable("id") Integer id, ModelMap model) {
+		model.addAttribute("cargo", service.find(id));
+		
+		return "/cargo/cadastro";
+	}
+	
+	@RequestMapping(value="editar", method=RequestMethod.POST)
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		service.update(cargo);
+		attr.addFlashAttribute("success", "Cargo editado com sucesso!");
+		
+		return "redirect:/cargos/listar";
 	}
 	
 }
